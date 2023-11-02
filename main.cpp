f2d92454784ac9433a1891532015e19739bb774e
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-11-02 20:12:55 +0100

    Fixed clang formatter

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello world" << std::endl;
-  std::cin.get();
+int main()
+{
+    std::cout << "Hello world" << std::endl; // flkajlfækjasækljfækasjd
+    std::cin.get(); // djskajksljdkla
 }
 
-struct MyStruct {
-  int thing, other, that;
+struct MyStruct             {
+    int thing,
+        other, that;
 };
 
 void LongFunctionNameVeryLongYesIndeed(int thisIsALongNameForAVariable,
                                        int ThisIsAnotherVeryLongVariableName);
 
-class Thing {
-  int a;
-  int b;
 
-  Thing(int in_a, int hjdfhjfdjshkjlfdhljhsdlajhfljkadjkshfjdashljfhaksjhfjkla)
-      : a(in_a), b(hjdfhjfdjshkjlfdhljhsdlajhfljkadjkshfjdashljfhaksjhfjkla){};
+
+
+
+
+
+
+
+class Thing
+{
+    int a;
+    int b;
+
+    private:
+    Thing(int in_a, int hjdfhjfdjshkjlfdhljhsdlajhfljkadjkshfjdashljfhaksjhfjkla)
+                                        : a(in_a), b(hjdfhjfdjshkjlfdhljhsdlajhfljkadjkshfjdashljfhaksjhfjkla){};
 };
